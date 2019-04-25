458a389bdb02c3524d9b03bc094816a1836ea299
Kevin Winata <kevinwinatamichael@gmail.com>
2019-04-25 17:08:23 +0800

    restructure to classes static method

--- reversi.py
+++ reversi.py
@@ -158,7 +158,6 @@ class Board:
     @staticmethod
     def getScoreOfBoard(board):
         # Determine the score by counting the tiles. Returns a dictionary with keys tile_1 and tile_2.
-        global scoretable
         xscore = 0
         oscore = 0
         for x in range(8):
@@ -207,226 +206,238 @@ class Board:
 
         return dupeBoard
 
-# create a table of score for each move in corresponding location
-scoretable=[]
-scoretable.append([99, -8, 8, 6, 6, 8, -8, 99])
-scoretable.append([-8, -24, -4, -3, -3, -4, -24, -8])
-scoretable.append([8, -4, 7, 4, 4, 7, -4, 8])
-scoretable.append([6, -3, 4, 0, 0, 4, -3, 6])
-scoretable.append([6, -3, 4, 0, 0, 4, -3, 6])
-scoretable.append([8, -4, 7, 4, 4, 7, -4, 8])
-scoretable.append([-8, -24, -4, -3, -3, -4, -24, -8])
-scoretable.append([99, -8, 8, 6, 6, 8, -8, 99])
 
-def enterPlayerTile():
-    # Lets the player type which tile they want to be.
-    # Returns a list with the player's tile as the first item, and the computer's tile as the second.
-    tile = ''
-    while not (tile == tile_1 or tile == tile_2):
-        print('Do you want to be %s or %s?' % (tile_1, tile_2))
-        tile = input().upper()
+class Interface:
+    @staticmethod
+    def enterPlayerTile():
+        # Lets the player type which tile they want to be.
+        # Returns a list with the player's tile as the first item, and the computer's tile as the second.
+        tile = ''
+        while not (tile == tile_1 or tile == tile_2):
+            print('Do you want to be %s or %s?' % (tile_1, tile_2))
+            tile = input().upper()
 
-    # the first element in the tuple is the player's tile, the second is the computer's tile.
-    if tile == tile_1:
-        return [tile_1, tile_2]
-    else:
-        return [tile_2, tile_1]
-
-
-def whoGoesFirst():
-    # Randomly choose the player who goes first.
-    if random.randint(0, 1) == 0:
-        return 'computer'
-    else:
-        return 'player'
-
-
-def playAgain():
-    # This function returns True if the player wants to play again, otherwise it returns False.
-    print('Do you want to play again? (yes or no)')
-    return input().lower().startswith('y')
-
-
-def getPlayerMove(board, playerTile):
-    # Let the player type in their move.
-    # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
-    DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
-    while True:
-        print('Enter your move, or type quit to end the game, or hints to turn off/on hints.')
-        move = input().lower()
-        if move == 'quit':
-            return 'quit'
-        if move == 'hints':
-            return 'hints'
-
-        if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
-            x = int(move[0]) - 1
-            y = int(move[1]) - 1
-            if Board.is_valid_move(board, playerTile, x, y) == False:
-                continue
-            else:
-                break
+        # the first element in the tuple is the player's tile, the second is the computer's tile.
+        if tile == tile_1:
+            return [tile_1, tile_2]
         else:
-            print('That is not a valid move. Type the x digit (1-8), then the y digit (1-8).')
-            print('For example, 81 will be the top-right corner.')
+            return [tile_2, tile_1]
 
-    return [x, y]
+    @staticmethod
+    def whoGoesFirst():
+        # Randomly choose the player who goes first.
+        if random.randint(0, 1) == 0:
+            return 'computer'
+        else:
+            return 'player'
 
-
-def getComputerMove(board, computerTile):
-    # Given a board and the computer's tile, determine where to
-    # move and return that move as a [x, y] list.
-
-    global scoretable
-
-    possibleMoves = Board.getValidMoves(board, computerTile)
-
-    # randomize the order of the possible moves
-    random.shuffle(possibleMoves)
-
-    # get the player tile (=oppTile)
-    if(computerTile==tile_1):
-        oppTile=tile_2
-    else:
-        oppTile=tile_1
-
-    # Go through all the possible moves and remember the best scoring move
-    bestScore = -1e5
-
-    #to avoid bestMove referenced in the return function call before it is initialized
-    try:
-        bestMove = possibleMoves[0]
-    except:
-        bestMove=None
-
-    for x, y in possibleMoves:
-
-        print("considering ", x+1, y+1) # ux
-        score = alphabeta(board, 5, -1e9, 1e9, computerTile, computerTile) # get the state of the best minimax board
-        score += scoretable[x][y] # add with the cost of the move
-
-        if score > bestScore:
-            bestMove = [x, y]
-            bestScore = score
-
-    return bestMove
-
-def alphabeta(board, depth, alpha, beta ,computerTile, tile):
-    # implementation of alphabeta pruning
-
-    global scoretable
-
-    possibleMoves = Board.getValidMoves(board, tile)
-
-    # check for terminal node
-    if depth == 0 or possibleMoves == []:
-        return Board.getScoreOfBoard(board)[computerTile]
-
-    # get the player tile
-    oppTile = "X"
-    if(tile=="X"):
-        oppTile = "O"
-
-    if tile==computerTile: #if maximizing
-        v = -1e9
-
-        for x, y in possibleMoves:
-
-            # get the alphabeta of child
-            child = Board.getBoardCopy(board)
-            Board.makeMove(child, tile, x, y)
-            v = alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
-            v += scoretable[x][y]
-
-            alpha = max(alpha, v)
-
-            if beta<=alpha:
-                break
-        return v
-    else: # if minimizing
-        v = 1e9
-
-        for x, y in possibleMoves:
-
-            # get the alphabeta of child
-            child = Board.getBoardCopy(board)
-            Board.makeMove(child, tile, x, y)
-            v = alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
-            v -= scoretable[x][y]
-
-            beta = min(beta, v)
-
-            if beta<=alpha:
-                break
-        return v
-
-def showPoints(playerTile, computerTile):
-    # Prints out the current score.
-    scores = Board.getPointBoard(mainBoard)
-    print('You have %s points. The computer has %s points.' % (scores[playerTile], scores[computerTile]))
+    @staticmethod
+    def playAgain():
+        # This function returns True if the player wants to play again, otherwise it returns False.
+        print('Do you want to play again? (yes or no)')
+        return input().lower().startswith('y')
 
 
+    @staticmethod
+    def getPlayerMove(board, playerTile):
+        # Let the player type in their move.
+        # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
+        DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
+        while True:
+            print('Enter your move, or type quit to end the game, or hints to turn off/on hints.')
+            move = input().lower()
+            if move == 'quit':
+                return 'quit'
+            if move == 'hints':
+                return 'hints'
 
-print('Welcome to Reversi!')
-
-while True:
-    # Reset the board and game.
-    mainBoard = Board.get_blank_board()
-    Board.reset_board(mainBoard)
-    playerTile, computerTile = enterPlayerTile()
-    showHints = False
-    turn = whoGoesFirst()
-    print('The ' + turn + ' will go first.')
-
-    while True:
-        if turn == 'player':
-            # Player's turn.
-            if showHints:
-                validMovesBoard = Board.get_board_with_hints(mainBoard, playerTile)
-                Board.draw_board(validMovesBoard)
-            else:
-                Board.draw_board(mainBoard)
-            if Board.getValidMoves(mainBoard, playerTile) == []:
-                print("No valid moves\n")
-            else:
-                showPoints(playerTile, computerTile)
-                move = getPlayerMove(mainBoard, playerTile)
-                if move == 'quit':
-                    print('Thanks for playing!')
-                    sys.exit() # terminate the program
-                elif move == 'hints':
-                    showHints = not showHints
+            if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
+                x = int(move[0]) - 1
+                y = int(move[1]) - 1
+                if Board.is_valid_move(board, playerTile, x, y) == False:
                     continue
                 else:
-                    Board.makeMove(mainBoard, playerTile, move[0], move[1])
-            turn = 'computer'
-
-        else:
-            # Computer's turn.
-            Board.draw_board(mainBoard)
-            if Board.getValidMoves(mainBoard, computerTile) == []:
-                if Board.getValidMoves(mainBoard, playerTile) == []:
-                    print("Game ends: No move possible")
                     break
-                print("No valid move\n")
             else:
-                showPoints(playerTile, computerTile)
-                print("I'm thinking...")
-                x, y = getComputerMove(mainBoard, computerTile)
-                Board.makeMove(mainBoard, computerTile, x, y)
-                print("My move: ", x+1, y+1)
-            turn = 'player'
+                print('That is not a valid move. Type the x digit (1-8), then the y digit (1-8).')
+                print('For example, 81 will be the top-right corner.')
 
-    # Display the final score.
-    Board.draw_board(mainBoard)
-    scores = Board.getPointBoard(mainBoard)
-    print('%s scored %s points. %s scored %s points.' % (tile_1, scores[tile_1], tile_2, scores[tile_2]))
-    if scores[playerTile] > scores[computerTile]:
-        print('You beat the computer by %s points! Congratulations!' % (scores[playerTile] - scores[computerTile]))
-    elif scores[playerTile] < scores[computerTile]:
-        print('You lost. The computer beat you by %s points.' % (scores[computerTile] - scores[playerTile]))
-    else:
-        print('The game was a tie!')
+        return [x, y]
 
-    if not playAgain():
-        break
+    @staticmethod
+    def getComputerMove(board, computerTile):
+        # Given a board and the computer's tile, determine where to
+        # move and return that move as a [x, y] list.
+
+        possibleMoves = Board.getValidMoves(board, computerTile)
+
+        # randomize the order of the possible moves
+        random.shuffle(possibleMoves)
+
+        # get the player tile (=oppTile)
+        if(computerTile==tile_1):
+            oppTile=tile_2
+        else:
+            oppTile=tile_1
+
+        # Go through all the possible moves and remember the best scoring move
+        bestScore = -1e5
+
+        #to avoid bestMove referenced in the return function call before it is initialized
+        try:
+            bestMove = possibleMoves[0]
+        except:
+            bestMove = None
+
+        for x, y in possibleMoves:
+
+            print("considering ", x+1, y+1) # ux
+            score = Algorithm.alphabeta(board, 5, -1e9, 1e9, computerTile, computerTile) # get the state of the best minimax board
+            score += Algorithm.scoretable[x][y] # add with the cost of the move
+
+            if score > bestScore:
+                bestMove = [x, y]
+                bestScore = score
+
+        return bestMove
+
+    @staticmethod
+    def show_points(playerTile, computerTile, mainBoard):
+        # Prints out the current score.
+        scores = Board.getPointBoard(mainBoard)
+        print('You have %s points. The computer has %s points.' % (scores[playerTile], scores[computerTile]))
+
+    @staticmethod
+    def main():
+        print('Welcome to Reversi!')
+
+        while True:
+            # Reset the board and game.
+            mainBoard = Board.get_blank_board()
+            Board.reset_board(mainBoard)
+            playerTile, computerTile = Interface.enterPlayerTile()
+            showHints = False
+            turn = Interface.whoGoesFirst()
+            print('The ' + turn + ' will go first.')
+
+            while True:
+                if turn == 'player':
+                    # Player's turn.
+                    if showHints:
+                        validMovesBoard = Board.get_board_with_hints(mainBoard, playerTile)
+                        Board.draw_board(validMovesBoard)
+                    else:
+                        Board.draw_board(mainBoard)
+                    if Board.getValidMoves(mainBoard, playerTile) == []:
+                        print("No valid moves\n")
+                    else:
+                        Interface.show_points(playerTile, computerTile, mainBoard)
+                        move = Interface.getPlayerMove(mainBoard, playerTile)
+                        if move == 'quit':
+                            print('Thanks for playing!')
+                            sys.exit()  # terminate the program
+                        elif move == 'hints':
+                            showHints = not showHints
+                            continue
+                        else:
+                            Board.makeMove(mainBoard, playerTile, move[0], move[1])
+                    turn = 'computer'
+
+                else:
+                    # Computer's turn.
+                    Board.draw_board(mainBoard)
+                    if Board.getValidMoves(mainBoard, computerTile) == []:
+                        if Board.getValidMoves(mainBoard, playerTile) == []:
+                            print("Game ends: No move possible")
+                            break
+                        print("No valid move\n")
+                    else:
+                        Interface.show_points(playerTile, computerTile, mainBoard)
+                        print("I'm thinking...")
+                        x, y = Interface.getComputerMove(mainBoard, computerTile)
+                        Board.makeMove(mainBoard, computerTile, x, y)
+                        print("My move: ", x + 1, y + 1)
+                    turn = 'player'
+
+            # Display the final score.
+            Board.draw_board(mainBoard)
+            scores = Board.getPointBoard(mainBoard)
+            print('%s scored %s points. %s scored %s points.' % (tile_1, scores[tile_1], tile_2, scores[tile_2]))
+            if scores[playerTile] > scores[computerTile]:
+                print('You beat the computer by %s points! Congratulations!' % (
+                            scores[playerTile] - scores[computerTile]))
+            elif scores[playerTile] < scores[computerTile]:
+                print('You lost. The computer beat you by %s points.' % (scores[computerTile] - scores[playerTile]))
+            else:
+                print('The game was a tie!')
+
+            if not Interface.playAgain():
+                break
+
+
+class Algorithm:
+    scoretable = [
+            [99, -8, 8, 6, 6, 8, -8, 99],
+            [-8, -24, -4, -3, -3, -4, -24, -8],
+            [8, -4, 7, 4, 4, 7, -4, 8],
+            [6, -3, 4, 0, 0, 4, -3, 6],
+            [6, -3, 4, 0, 0, 4, -3, 6],
+            [8, -4, 7, 4, 4, 7, -4, 8],
+            [-8, -24, -4, -3, -3, -4, -24, -8],
+            [99, -8, 8, 6, 6, 8, -8, 99],
+        ]
+
+    @staticmethod
+    def alphabeta(board, depth, alpha, beta ,computerTile, tile):
+        # implementation of alphabeta pruning
+
+
+        possibleMoves = Board.getValidMoves(board, tile)
+
+        # check for terminal node
+        if depth == 0 or possibleMoves == []:
+            return Board.getScoreOfBoard(board)[computerTile]
+
+        # get the player tile
+        oppTile = "X"
+        oppTile = "X"
+        if(tile=="X"):
+            oppTile = "O"
+
+        if tile==computerTile: #if maximizing
+            v = -1e9
+
+            for x, y in possibleMoves:
+
+                # get the alphabeta of child
+                child = Board.getBoardCopy(board)
+                Board.makeMove(child, tile, x, y)
+                v = Algorithm.alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
+                v += Algorithm.scoretable[x][y]
+
+                alpha = max(alpha, v)
+
+                if beta<=alpha:
+                    break
+            return v
+        else:  # if minimizing
+            v = 1e9
+
+            for x, y in possibleMoves:
+
+                # get the alphabeta of child
+                child = Board.getBoardCopy(board)
+                Board.makeMove(child, tile, x, y)
+                v = Algorithm.alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
+                v -= Algorithm.scoretable[x][y]
+
+                beta = min(beta, v)
+
+                if beta<=alpha:
+                    break
+            return v
+
+Interface.main()
+
+
+
