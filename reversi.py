a5850f9cfcaf846e1b91c8c27873731a18c1c3ef
Kevin Winata <kevinwinatamichael@gmail.com>
2019-04-25 16:50:24 +0800

    move methods to class Board

--- reversi.py
+++ reversi.py
@@ -39,117 +39,173 @@ class Location:
     def copy(self):
         return Location(self.x, self.y)
 
+    def is_on_board(self):
+        return 0 <= self.x <= 7 and 0 <= self.y <= 7
 
-def draw_board(board):
-    horizontal_line = '  ---------------------------------'
 
-    print()
-    print('    1   2   3   4   5   6   7   8')
-    print(horizontal_line)
-    for y in range(8):
-        print(y+1, end=' ')
-        for x in range(8):
-            print('| ' + board[x][y], end=' ')
-        print('| ' + str(y+1))
+class Board:
+    @staticmethod
+    def draw_board(board):
+        horizontal_line = '  ---------------------------------'
+
+        print()
+        print('    1   2   3   4   5   6   7   8')
         print(horizontal_line)
-    print('    1   2   3   4   5   6   7   8')
-    print()
-
-
-def reset_board(board):
-    for x in range(8):
         for y in range(8):
-            board[x][y] = ' '
+            print(y + 1, end=' ')
+            for x in range(8):
+                print('| ' + board[x][y], end=' ')
+            print('| ' + str(y + 1))
+            print(horizontal_line)
+        print('    1   2   3   4   5   6   7   8')
+        print()
 
-    # Starting pieces
-    board[3][3] = tile_1
-    board[3][4] = tile_2
-    board[4][3] = tile_2
-    board[4][4] = tile_1
+    @staticmethod
+    def reset_board(board):
+        for x in range(8):
+            for y in range(8):
+                board[x][y] = ' '
 
+        # Starting pieces
+        board[3][3] = tile_1
+        board[3][4] = tile_2
+        board[4][3] = tile_2
+        board[4][4] = tile_1
 
-def get_blank_board():
-    board = []
+    @staticmethod
+    def get_blank_board():
+        board = []
 
-    for i in range(8):
-        board.append([' '] * 8)
+        for i in range(8):
+            board.append([' '] * 8)
 
-    return board
+        return board
 
+    @staticmethod
+    def check_valid_location(board, x, y):
+        if board[x][y] != ' ' or not Location(x, y).is_on_board():
+            raise InvalidLocationException()
 
-def check_valid_location(board, x, y):
-    if board[x][y] != ' ' or not is_on_board(x, y):
-        raise InvalidLocationException()
+    @staticmethod
+    def get_tiles_to_flip_for_move(board, xstart, ystart, tile):
+        if tile == tile_1:
+            other_tile = tile_2
+        else:
+            other_tile = tile_1
 
+        start_loc = Location(xstart, ystart)
 
-def get_tiles_to_flip_for_move(board, xstart, ystart, tile):
-    if tile == tile_1:
-        other_tile = tile_2
-    else:
-        other_tile = tile_1
-
-    start_loc = Location(xstart, ystart)
-
-    tiles_to_flip = []
-    for direction in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
-        current_loc = start_loc.copy()
-        current_loc.offset(direction)
-        while is_on_board(current_loc.x, current_loc.y) and board[current_loc.x][current_loc.y] == other_tile:
+        tiles_to_flip = []
+        for direction in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+            current_loc = start_loc.copy()
             current_loc.offset(direction)
-        if not is_on_board(current_loc.x, current_loc.y):
-            continue
-        if board[current_loc.x][current_loc.y] == tile:
-            current_loc.offset(direction, reverse=True)
-            while current_loc != start_loc:
-                tiles_to_flip.append([current_loc.x, current_loc.y])
+            while current_loc.is_on_board() and board[current_loc.x][current_loc.y] == other_tile:
+                current_loc.offset(direction)
+            if not current_loc.is_on_board():
+                continue
+            if board[current_loc.x][current_loc.y] == tile:
                 current_loc.offset(direction, reverse=True)
-    return tiles_to_flip
+                while current_loc != start_loc:
+                    tiles_to_flip.append([current_loc.x, current_loc.y])
+                    current_loc.offset(direction, reverse=True)
+        return tiles_to_flip
 
+    @staticmethod
+    def get_move_result(board, tile, xstart, ystart):
+        # Returns False if the player's move on space xstart, ystart is invalid.
+        # If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
 
-def get_move_result(board, tile, xstart, ystart):
-    # Returns False if the player's move on space xstart, ystart is invalid.
-    # If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
+        try:
+            Board.check_valid_location(board, xstart, ystart)
+        except:
+            return False
 
-    try:
-        check_valid_location(board, xstart, ystart)
-    except:
+        tiles_to_flip = Board.get_tiles_to_flip_for_move(board, xstart, ystart, tile)
+
+        if len(tiles_to_flip) == 0: # If no tiles were flipped, this is not a valid move.
+            return False
+        return tiles_to_flip
+
+    @staticmethod
+    def is_valid_move(board, tile, xstart, ystart):
+        if Board.get_move_result(board, tile, xstart, ystart):
+            return True
         return False
 
-    tiles_to_flip = get_tiles_to_flip_for_move(board, xstart, ystart, tile)
-
-    if len(tiles_to_flip) == 0: # If no tiles were flipped, this is not a valid move.
-        return False
-    return tiles_to_flip
 
 
-def is_valid_move(board, tile, xstart, ystart):
-    if get_move_result(board, tile, xstart, ystart):
+    @staticmethod
+    def get_board_with_hints(board, tile):
+        new_board_for_printing_only = Board.getBoardCopy(board)
+
+        for x, y in Board.getValidMoves(new_board_for_printing_only, tile):
+            new_board_for_printing_only[x][y] = tile_hint
+
+        return new_board_for_printing_only
+
+    @staticmethod
+    def getValidMoves(board, tile):
+        # Returns a list of [x,y] lists of valid moves for the given player on the given board.
+        validMoves = []
+
+        for x in range(8):
+            for y in range(8):
+                if Board.is_valid_move(board, tile, x, y) != False:
+                    validMoves.append([x, y])
+        return validMoves
+
+
+    @staticmethod
+    def getScoreOfBoard(board):
+        # Determine the score by counting the tiles. Returns a dictionary with keys tile_1 and tile_2.
+        global scoretable
+        xscore = 0
+        oscore = 0
+        for x in range(8):
+            for y in range(8):
+                if board[x][y] == tile_1:
+                    xscore += 3
+                if board[x][y] == tile_2:
+                    oscore += 3
+        return {tile_1:xscore, tile_2:oscore}
+
+    @staticmethod
+    def getPointBoard(board):
+        # Determine the score by counting the tiles. Returns a dictionary with keys tile_1 and tile_2.
+        xscore = 0
+        oscore = 0
+        for x in range(8):
+            for y in range(8):
+                if board[x][y] == tile_1:
+                    xscore += 1
+                if board[x][y] == tile_2:
+                    oscore += 1
+        return {tile_1:xscore, tile_2:oscore}
+
+    @staticmethod
+    def makeMove(board, tile, xstart, ystart):
+        # Place the tile on the board at xstart, ystart, and flip any of the opponent's pieces.
+        # Returns False if this is an invalid move, True if it is valid.
+        tiles_to_flip = Board.get_move_result(board, tile, xstart, ystart)
+
+        if tiles_to_flip == False:
+            return False
+
+        board[xstart][ystart] = tile
+        for x, y in tiles_to_flip:
+            board[x][y] = tile
         return True
-    return False
 
+    @staticmethod
+    def getBoardCopy(board):
+        # Make a duplicate of the board list and return the duplicate.
+        dupeBoard = Board.get_blank_board()
 
-def is_on_board(x, y):
-    return 0 <= x <= 7 and 0 <= y <= 7
+        for x in range(8):
+            for y in range(8):
+                dupeBoard[x][y] = board[x][y]
 
-
-def get_board_with_hints(board, tile):
-    new_board_for_printing_only = getBoardCopy(board)
-
-    for x, y in getValidMoves(new_board_for_printing_only, tile):
-        new_board_for_printing_only[x][y] = tile_hint
-
-    return new_board_for_printing_only
-
-
-def getValidMoves(board, tile):
-    # Returns a list of [x,y] lists of valid moves for the given player on the given board.
-    validMoves = []
-
-    for x in range(8):
-        for y in range(8):
-            if is_valid_move(board, tile, x, y) != False:
-                validMoves.append([x, y])
-    return validMoves
+        return dupeBoard
 
 # create a table of score for each move in corresponding location
 scoretable=[]
@@ -161,32 +217,6 @@ scoretable.append([6, -3, 4, 0, 0, 4, -3, 6])
 scoretable.append([8, -4, 7, 4, 4, 7, -4, 8])
 scoretable.append([-8, -24, -4, -3, -3, -4, -24, -8])
 scoretable.append([99, -8, 8, 6, 6, 8, -8, 99])
-
-def getScoreOfBoard(board):
-    # Determine the score by counting the tiles. Returns a dictionary with keys tile_1 and tile_2.
-    global scoretable
-    xscore = 0
-    oscore = 0
-    for x in range(8):
-        for y in range(8):
-            if board[x][y] == tile_1:
-                xscore += 3
-            if board[x][y] == tile_2:
-                oscore += 3
-    return {tile_1:xscore, tile_2:oscore}
-
-def getPointBoard(board):
-    # Determine the score by counting the tiles. Returns a dictionary with keys tile_1 and tile_2.
-    xscore = 0
-    oscore = 0
-    for x in range(8):
-        for y in range(8):
-            if board[x][y] == tile_1:
-                xscore += 1
-            if board[x][y] == tile_2:
-                oscore += 1
-    return {tile_1:xscore, tile_2:oscore}
-
 
 def enterPlayerTile():
     # Lets the player type which tile they want to be.
@@ -217,31 +247,6 @@ def playAgain():
     return input().lower().startswith('y')
 
 
-def makeMove(board, tile, xstart, ystart):
-    # Place the tile on the board at xstart, ystart, and flip any of the opponent's pieces.
-    # Returns False if this is an invalid move, True if it is valid.
-    tiles_to_flip = get_move_result(board, tile, xstart, ystart)
-
-    if tiles_to_flip == False:
-        return False
-
-    board[xstart][ystart] = tile
-    for x, y in tiles_to_flip:
-        board[x][y] = tile
-    return True
-
-
-def getBoardCopy(board):
-    # Make a duplicate of the board list and return the duplicate.
-    dupeBoard = get_blank_board()
-
-    for x in range(8):
-        for y in range(8):
-            dupeBoard[x][y] = board[x][y]
-
-    return dupeBoard
-
-
 def getPlayerMove(board, playerTile):
     # Let the player type in their move.
     # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
@@ -257,7 +262,7 @@ def getPlayerMove(board, playerTile):
         if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
             x = int(move[0]) - 1
             y = int(move[1]) - 1
-            if is_valid_move(board, playerTile, x, y) == False:
+            if Board.is_valid_move(board, playerTile, x, y) == False:
                 continue
             else:
                 break
@@ -274,7 +279,7 @@ def getComputerMove(board, computerTile):
 
     global scoretable
 
-    possibleMoves = getValidMoves(board, computerTile)
+    possibleMoves = Board.getValidMoves(board, computerTile)
 
     # randomize the order of the possible moves
     random.shuffle(possibleMoves)
@@ -311,11 +316,11 @@ def alphabeta(board, depth, alpha, beta ,computerTile, tile):
 
     global scoretable
 
-    possibleMoves = getValidMoves(board, tile)
+    possibleMoves = Board.getValidMoves(board, tile)
 
     # check for terminal node
     if depth == 0 or possibleMoves == []:
-        return getScoreOfBoard(board)[computerTile]
+        return Board.getScoreOfBoard(board)[computerTile]
 
     # get the player tile
     oppTile = "X"
@@ -328,8 +333,8 @@ def alphabeta(board, depth, alpha, beta ,computerTile, tile):
         for x, y in possibleMoves:
 
             # get the alphabeta of child
-            child = getBoardCopy(board)
-            makeMove(child, tile, x, y)
+            child = Board.getBoardCopy(board)
+            Board.makeMove(child, tile, x, y)
             v = alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
             v += scoretable[x][y]
 
@@ -344,8 +349,8 @@ def alphabeta(board, depth, alpha, beta ,computerTile, tile):
         for x, y in possibleMoves:
 
             # get the alphabeta of child
-            child = getBoardCopy(board)
-            makeMove(child, tile, x, y)
+            child = Board.getBoardCopy(board)
+            Board.makeMove(child, tile, x, y)
             v = alphabeta(child, depth-1, alpha, beta, computerTile, oppTile)
             v -= scoretable[x][y]
 
@@ -357,7 +362,7 @@ def alphabeta(board, depth, alpha, beta ,computerTile, tile):
 
 def showPoints(playerTile, computerTile):
     # Prints out the current score.
-    scores = getPointBoard(mainBoard)
+    scores = Board.getPointBoard(mainBoard)
     print('You have %s points. The computer has %s points.' % (scores[playerTile], scores[computerTile]))
 
 
@@ -366,8 +371,8 @@ print('Welcome to Reversi!')
 
 while True:
     # Reset the board and game.
-    mainBoard = get_blank_board()
-    reset_board(mainBoard)
+    mainBoard = Board.get_blank_board()
+    Board.reset_board(mainBoard)
     playerTile, computerTile = enterPlayerTile()
     showHints = False
     turn = whoGoesFirst()
@@ -377,11 +382,11 @@ while True:
         if turn == 'player':
             # Player's turn.
             if showHints:
-                validMovesBoard = get_board_with_hints(mainBoard, playerTile)
-                draw_board(validMovesBoard)
+                validMovesBoard = Board.get_board_with_hints(mainBoard, playerTile)
+                Board.draw_board(validMovesBoard)
             else:
-                draw_board(mainBoard)
-            if getValidMoves(mainBoard, playerTile) == []:
+                Board.draw_board(mainBoard)
+            if Board.getValidMoves(mainBoard, playerTile) == []:
                 print("No valid moves\n")
             else:
                 showPoints(playerTile, computerTile)
@@ -393,14 +398,14 @@ while True:
                     showHints = not showHints
                     continue
                 else:
-                    makeMove(mainBoard, playerTile, move[0], move[1])
+                    Board.makeMove(mainBoard, playerTile, move[0], move[1])
             turn = 'computer'
 
         else:
             # Computer's turn.
-            draw_board(mainBoard)
-            if getValidMoves(mainBoard, computerTile) == []:
-                if getValidMoves(mainBoard, playerTile) == []:
+            Board.draw_board(mainBoard)
+            if Board.getValidMoves(mainBoard, computerTile) == []:
+                if Board.getValidMoves(mainBoard, playerTile) == []:
                     print("Game ends: No move possible")
                     break
                 print("No valid move\n")
@@ -408,13 +413,13 @@ while True:
                 showPoints(playerTile, computerTile)
                 print("I'm thinking...")
                 x, y = getComputerMove(mainBoard, computerTile)
-                makeMove(mainBoard, computerTile, x, y)
+                Board.makeMove(mainBoard, computerTile, x, y)
                 print("My move: ", x+1, y+1)
             turn = 'player'
 
     # Display the final score.
-    draw_board(mainBoard)
-    scores = getPointBoard(mainBoard)
+    Board.draw_board(mainBoard)
+    scores = Board.getPointBoard(mainBoard)
     print('%s scored %s points. %s scored %s points.' % (tile_1, scores[tile_1], tile_2, scores[tile_2]))
     if scores[playerTile] > scores[computerTile]:
         print('You beat the computer by %s points! Congratulations!' % (scores[playerTile] - scores[computerTile]))
