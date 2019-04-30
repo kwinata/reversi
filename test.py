b8d919a6d91151cf03657e589f6d0dc7c54c36f1
Kevin Winata <kevinwinatamichael@gmail.com>
2019-04-30 16:51:52 +0800

    restructure test

--- test.py
+++ test.py
@@ -6,11 +6,17 @@ tile_1 = '#'
 tile_2 = '.'
 tile_hint = '?'
 
+
 class InterfaceTest(TestCase):
 
-    def test_play(self):
-        MyInput.args = [tile_1, "53", "64", "33", "quit"]
+    def play(self, args, expPlayerScore, expComputerScore):
+        MyInput.args = args
         Interface.main()
         scores = Interface.mainBoard.getPointBoard()
-        self.assertEqual(4, scores[tile_1])
-        self.assertEqual(6, scores[tile_2])
+        self.assertEqual(expPlayerScore, scores[tile_1])
+        self.assertEqual(expComputerScore, scores[tile_2])
+
+    def test_play(self):
+        self.play([tile_1, "53", "64", "33", "quit"], 4, 6)
+        self.play([tile_1, "35", "37", "88", "63", "27", "18", "quit"], 6, 8)
+        self.play(["53", tile_1, "53", "cat", "quit"], 3, 3)
